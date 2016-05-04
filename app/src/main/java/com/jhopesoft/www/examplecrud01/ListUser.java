ed2cd6edb37c1f439978aa54f1359398a4b7bf9f
jhopes <jhopes.p@gmail.com>
2016-05-04 18:55:52 -0500

    crud3

--- app/src/main/java/com/jhopesoft/www/examplecrud01/ListUser.java
+++ app/src/main/java/com/jhopesoft/www/examplecrud01/ListUser.java
@@ -1,5 +1,7 @@
 package com.jhopesoft.www.examplecrud01;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,13 +11,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewAnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import Controller.ClsUser;
+import Modell.User;
 
 public class ListUser extends AppCompatActivity {
 
     ListView list_user;
+    User u;
+    ClsUser cu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +30,61 @@ public class ListUser extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         list_user = (ListView) findViewById(R.id.ListvUser);
+        u = new User();
+        cu = new ClsUser(ListUser.this);
         ListUser();
 
+        list_user.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(final AdapterView<?> parent, View view, final int position, final long _id) {
+
+                final String[] items = {"Editar", "Eliminar"};
+
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(ListUser.this);
+
+                builder.setTitle("Selección")
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int item) {
+                                switch (item){
+                                    case 0:
+                                        Toast.makeText(getApplicationContext(),"edit",Toast.LENGTH_SHORT).show();
+                                        break;
+                                    case 1:
+                                        AlertDialog.Builder builder =
+                                                new AlertDialog.Builder(ListUser.this);
+
+                                        builder.setMessage("¿Esta seguro que desea eliminar?")
+                                                .setTitle("Confirmación")
+                                                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int id) {
+                                                       //eliminar
+                                                        u.setId(_id+"");
+                                                        if(cu.DeleteUser(u)){
+                                                            ListUser();
+                                                            Toast.makeText(ListUser.this, "Eliminado", Toast.LENGTH_SHORT).show();
+                                                        }else{
+                                                            Toast.makeText(ListUser.this, "Error de eliminación", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                       dialog.cancel();
+                                                    }
+                                                })
+                                                .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int id) {
+                                                        dialog.cancel();
+                                                    }
+                                                });
+
+                                        builder.create().show();
+                                        break;
+                                }
+                            }
+                        });
+                builder.create().show();
+            }
+        });
     }
     public void ListUser(){
-        ClsUser cu = new ClsUser(getApplicationContext());
         String[] columnas = new String[]{"campo1","campo2"};
         int[] id_views = new int[]{android.R.id.text1, android.R.id.text2};
         Cursor listuser = cu.readUser();
